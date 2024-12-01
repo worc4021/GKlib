11aac5d761793422696a12d33fba7fd91f07566a
Manuel Schaich <rmschaich@outlook.com>
2024-12-01 11:35:23 +0000

    Use specific install directory

--- .github/actions/install-oneAPI.bat
+++ .github/actions/install-oneAPI.bat
@@ -6,13 +6,13 @@ set URL=%1
 set COMPONENTS=%2
 
 curl.exe --output %TEMP%\webimage.exe --url %URL% --retry 5 --retry-delay 5
-start /b /wait %TEMP%\webimage.exe -s -x -f webimage_extracted --log extract.log
+start /b /wait %TEMP%\webimage.exe -s -x -f %TEMP%\webimage_extracted --log extract.log
 del %TEMP%\webimage.exe
 if "%COMPONENTS%"=="" (
-  webimage_extracted\bootstrapper.exe -s --action install --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
+  %TEMP%\webimage_extracted\bootstrapper.exe -s --action install --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --install-dir=%TEMP%\oneAPI --log-dir=.
 ) else (
-  webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --log-dir=.
+  %TEMP%\webimage_extracted\bootstrapper.exe -s --action install --components=%COMPONENTS% --eula=accept -p=NEED_VS2017_INTEGRATION=0 -p=NEED_VS2019_INTEGRATION=0 -p=NEED_VS2022_INTEGRATION=0 --install-dir=%TEMP%\oneAPI --log-dir=.
 )
 set installer_exit_code=%ERRORLEVEL%
-rd /s/q "webimage_extracted"
+rd /s/q "%TEMP%\webimage_extracted"
 exit /b %installer_exit_code%
